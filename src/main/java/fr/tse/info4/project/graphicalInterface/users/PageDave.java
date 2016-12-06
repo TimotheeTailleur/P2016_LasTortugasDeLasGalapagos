7556b8f511ee3ae854d2d2785370dd15b676bc6b
Pierre Vassal <pvassal.94@gmail.com>
2016-12-06 23:45:33 +0100

    compter combien de tags entre l'utilisateur correction de la taille du JTextField Architecture des ActionListener pour les boutons 1 ( fonctionnalités 1 et 3 ) et 2 ( fonctionnalité 2)

--- src/main/java/fr/tse/info4/project/graphicalInterface/users/PageDave.java
+++ src/main/java/fr/tse/info4/project/graphicalInterface/users/PageDave.java
@@ -1,5 +1,8 @@
 package fr.tse.info4.project.graphicalInterface.users;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JPanel;
@@ -7,17 +10,23 @@ import javax.swing.JTextField;
 
 import fr.tse.info4.project.graphicalInterface.ref.TabReference;
 
+import fr.tse.info4.project.user.Dave;
+
 
 public class PageDave extends TabReference {
 
-	JTextField tagName = new JTextField("");
+	JTextField tagName = new JTextField(25);
+	
 
+/**
+ * 
+ */
 	public PageDave() {
 		super();
 
 		JPanel text = new JPanel();
-
-		// tagName.setSize(40, 20); //marche pas
+		tagName.setHorizontalAlignment(JTextField.CENTER);
+		
 		JPanel boutons = new JPanel();
 		boutons.setLayout(new BoxLayout(boutons, BoxLayout.LINE_AXIS));
 
@@ -26,9 +35,46 @@ public class PageDave extends TabReference {
 
 		boutons.add(option1);
 		boutons.add(option2);
+		
+		option1.addActionListener(new ActionListener(){
+			 public void actionPerformed(ActionEvent event){
+			
+				 String nomTag = tagName.getText(); 
+				 int longueur=0;
+				 
+				 for(int i=0; i<=nomTag.length()-1; i++) { // count how many tag were entered by the user
+
+					        if(nomTag.charAt(i)==' '){
+
+					          longueur++;
+					        }  
+					
+					      }                
+			        longueur+=1;
+					  
+				 
+				 if (longueur == 1){ // if one tag was written, launch function 1
+					
+				 }
+				 else { // else launch function 3
+					 nomTag.split(",;/:?!\\\n");					 
+				 }
+		}
+		});
+		
+
+		option1.addActionListener(new ActionListener(){
+			 public void actionPerformed(ActionEvent event){
+				 String nomTag = tagName.getText(); // read the tad written by the user and launch function 2
+				 
+				 }
+		});
+		
+			 
+		
 
 		text.add(tagName);
-		text.setSize(1000, 900);// marche pas
+		text.setSize(1000, 900);
 		this.getPanel().add(text);
 		this.getPanel().add(boutons);
 	}
@@ -42,4 +88,6 @@ public class PageDave extends TabReference {
 		}
 		return isList;
 	}
+	
+	
 }
