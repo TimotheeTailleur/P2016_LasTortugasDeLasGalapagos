0a9d457c040097d2ccde37437ab8c04fc0b53567
TimotheeTailleur <timothee.tailleur@telecom-st-etienne.fr>
2016-12-12 23:42:12 +0100

    Première implémentation Bob 1 & 2
    Reste à gérer les exceptions/ si l'utilisateur rentre n'importequoi

--- src/main/java/fr/tse/info4/project/datarecovery/BobApiManager.java
+++ src/main/java/fr/tse/info4/project/datarecovery/BobApiManager.java
@@ -1,21 +1,46 @@
 package fr.tse.info4.project.datarecovery;
 
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
+import java.util.List;
 
-
+import com.google.code.stackexchange.client.constant.ApplicationConstants;
+import com.google.code.stackexchange.client.provider.url.ApiUrlBuilder;
 import com.google.code.stackexchange.client.query.StackExchangeApiQueryFactory;
 import com.google.code.stackexchange.client.query.impl.TagApiQueryImpl;
 import com.google.code.stackexchange.common.PagedList;
 import com.google.code.stackexchange.schema.Paging;
 import com.google.code.stackexchange.schema.Question;
+import com.google.code.stackexchange.schema.StackExchangeSite;
 import com.google.code.stackexchange.schema.Tag;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
-public class BobApiManager {
+import fr.tse.info4.project.main.Main;
+import fr.tse.info4.project.schema.TagScore;
+
+public class BobApiManager extends ApiManager {
 
 	
+	public static final String SEARCH_SIMILAR_QUESTIONS = "com.google.code.stackexchange.client.searchSimilarQuestions";
+	
+	public static final String SIMILAR_QUESTIONS_FILTER = "!bA1d_KFuD9Rmoo";
+	
+	/**
+	 * Constructor to use applicationKey and specify StackExchangeSite
+	 * @param applicationKey
+	 * @param site
+	 */
+	public BobApiManager(String applicationKey, StackExchangeSite site) {
+		super(applicationKey, site);
+	}
 
 	/**
-	 * Return the new questions in the tag passed as parameter. <br>
+	 * Return new questions in the tag passed as parameter. <br>
 	 * The returned questions have at least one answer
 	 * 
 	 * @param tag
@@ -53,4 +78,97 @@ public class BobApiManager {
 		return questionsAnswered;
 	}
 
+	/**
+	 * Find key words (tags) to suggest based on the title of the question the user wants to submit
+	 * @param questionTitle
+	 * @return list of String Objects
+	 */
+	public ArrayList<String> findKeyWords(String questionTitle)
+	{
+		ArrayList<String> relatedTags = new ArrayList<String>();
+		
+		JsonArray elements = getJsonArrayWithSimilarQuestions(questionTitle,30);
+		
+		for (JsonElement o : elements)
+		{
+			JsonObject jsonObject = o.getAsJsonObject();
+			JsonElement tagNames = jsonObject.get("tags");
+			String[] tags = tagNames.toString().replace('[',' ').replace(']',' ').trim().split(",");
+			for (String s : tags)
+			{
+				if (!relatedTags.contains(s))
+				{
+					relatedTags.add(s);
+				}
+			}
+		}	
+		return relatedTags;
+	}
+	
+	/**
+	 * Find nbQuestions similar questions to the question the user wants to submit
+	 * @param questionTitle
+	 * @param nbQuestions Number of Questions you wish to look for (limited to 100)
+	 * @return list of Question Objects
+	 */
+	public ArrayList<Question> findSimilarQuestions(String questionTitle,int nbQuestions)
+	{
+		if (nbQuestions > 100)
+		{
+			nbQuestions=100;
+		}
+		ArrayList<Question> similarQuestions = new ArrayList<Question>(nbQuestions);
+
+		JsonArray elements = getJsonArrayWithSimilarQuestions(questionTitle,nbQuestions);
+		
+		for (JsonElement o : elements)
+		{
+			JsonObject jsonObject = o.getAsJsonObject();
+			int id = Integer.parseInt(jsonObject.get("question_id").toString());
+			String title = jsonObject.get("title").toString();
+			Question currentQuestion = new Question();
+			currentQuestion.setTitle(title); currentQuestion.setQuestionId(id);
+			similarQuestions.add(currentQuestion);
+		}
+		return similarQuestions;
+	}
+	
+	/**
+	 * Builds Url to get similar Questions, calls the Api and returns a JsonArray to manipulate data
+	 * @param questionTitle Title of the question the user wishes to submit
+	 * @param nbQuestions Number of similar questions to get (limited to 100)
+	 * @return
+	 */
+	public JsonArray getJsonArrayWithSimilarQuestions(String questionTitle,int nbQuestions)
+	{
+		ApiUrlBuilder builder = createStackOverflowApiUrlBuilder(SEARCH_SIMILAR_QUESTIONS).withParameter("title",questionTitle)
+				.withParameter("pagesize",new Integer(nbQuestions).toString()).withFilter(SIMILAR_QUESTIONS_FILTER);
+		String apiUrl = builder.buildUrl();
+		apiUrl+="&sort=relevance&order=desc";
+		
+		Charset UTF_8_CHAR_SET = Charset.forName(ApplicationConstants.CONTENT_ENCODING);
+		JsonParser parser = new JsonParser();
+		InputStream jsonContent = callApiMethod(apiUrl);
+		JsonElement response = parser.parse(new InputStreamReader(jsonContent, UTF_8_CHAR_SET));
+		JsonObject adaptee = response.getAsJsonObject();
+		return adaptee.get("items").getAsJsonArray();
+	}
+	
+	public static void main(String[] args) {
+		BobApiManager manager = new BobApiManager(APP_KEY, SITE);
+		ArrayList<String> relatedTags = manager.findKeyWords("vector c++");
+		System.out.println("Tags suggérés pour une question dont le titre est : \" vector c++ \" ");
+		for (String s : relatedTags)
+		{
+			System.out.println(s);
+		}
+		
+		ArrayList<Question> similarQuestions = manager.findSimilarQuestions("node js",20);
+		System.out.println("Questions similaires à \" node js\" ");
+		for (Question q : similarQuestions)
+		{
+			System.out.println(q.getQuestionId());
+			System.out.println(q.getTitle());
+		}
+	}
 }
