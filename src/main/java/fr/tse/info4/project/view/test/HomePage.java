f0f4746c7848a840267958bdf7bc7c1121a21f31
Boucard Delphine <delphine96.boucard@orange.fr>
2017-01-21 00:06:10 +0100

    Message de bienvenue pour cas acessToken + ajout de méthode la méthode fait un switch case pour modifier les onglets (dans les 2 cas)

--- src/main/java/fr/tse/info4/project/view/test/HomePage.java
+++ src/main/java/fr/tse/info4/project/view/test/HomePage.java
@@ -1,11 +1,8 @@
 package fr.tse.info4.project.view.test;
 
-import java.awt.BorderLayout;
-import java.awt.Component;
 import java.io.IOException;
 import java.net.URISyntaxException;
 
-import javax.swing.BoxLayout;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -15,7 +12,6 @@ import javax.swing.event.ChangeListener;
 
 import fr.tse.info4.project.model.datarecovery.ApiManager;
 import fr.tse.info4.project.view.ref.PageConnexion;
-import fr.tse.info4.project.view.ref.TabReference;
 import fr.tse.info4.project.view.users.*;
 
 public class HomePage extends JFrame {
@@ -36,14 +32,25 @@ public class HomePage extends JFrame {
 		JTabbedPane onglets = new JTabbedPane();
 		
 		JPanel wellcome =new JPanel();
-		wellcome.add(new JLabel("Bienvenue "));
+		JLabel hello = new JLabel("Bienvenue "+(new ApiManager()).getUserNAme((int)ApiManager.getIdUser(acessToken)));
+		wellcome.add(hello);
 		
 		onglets.addTab("Bienvenue", wellcome);
 
-		onglets.addTab("Alice", new PageAlice(acessToken));
-		onglets.addTab("Bob", new PageBob(acessToken));
+		onglets.addTab("Alice", new JPanel());
+		onglets.addTab("Bob", new JPanel());
 		//  onglets.addTab("Charlie", new PageCharlie(acessToken));
 		onglets.addTab("Dave", new PageDave());
+		
+		onglets.addChangeListener(new ChangeListener(){
+
+			@Override
+			public void stateChanged(ChangeEvent arg0) {
+				// TODO Auto-generated method stub
+				changeTab(onglets,acessToken,0);
+			}
+			
+		});
 
 		this.getContentPane().add(onglets);
 		this.setTitle("Las Tortugas De Las Galapagos - Projet Informatique - API");
@@ -70,29 +77,7 @@ public class HomePage extends JFrame {
 			@Override
 			public void stateChanged(ChangeEvent arg0) {
 				// TODO Auto-generated method stub
-				int index = onglets.getSelectedIndex();
-				switch(index){
-				case 1:
-					JPanel alice = (JPanel) onglets.getComponentAt(1);
-					alice.removeAll();
-					try {
-						alice.add(new PageAlice(id));
-					} catch (IOException | URISyntaxException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-					break;
-				case 2:
-					JPanel bob = (JPanel) onglets.getComponentAt(2);
-					bob.removeAll();
-					try {
-						bob.add(new PageBob(id));
-					} catch (IOException | URISyntaxException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-					break;
-				}
+				changeTab(onglets,null,id);
 			}
 			
 		});
@@ -103,6 +88,42 @@ public class HomePage extends JFrame {
 		this.setVisible(true);
 	}
 
+	
+	public void changeTab(JTabbedPane onglets,String acess, int id){
+		int index = onglets.getSelectedIndex();
+		switch(index){
+		case 1:
+			JPanel alice = (JPanel) onglets.getComponentAt(1);
+			alice.removeAll();
+			try {
+				if(acess==null){
+					alice.add(new PageAlice(id));
+				}else{
+					alice.add(new PageAlice(acess));
+				}
+				
+			} catch (IOException | URISyntaxException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			break;
+		case 2:
+			JPanel bob = (JPanel) onglets.getComponentAt(2);
+			bob.removeAll();
+			try {
+				if(acess==null){
+					bob.add(new PageBob(id));
+				}else{
+					bob.add(new PageBob(acess));
+				}
+			} catch (IOException | URISyntaxException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			break;
+		}
+		
+	}
 
 	public static void main(String[] args) throws IOException {
 		HomePage page = new HomePage();
