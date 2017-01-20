4d3057acc836bfab2a0f4422a1a418815bd13afa
Boucard Delphine <delphine96.boucard@orange.fr>
2017-01-21 00:20:35 +0100

    Plus d'attente de chargement

--- src/main/java/fr/tse/info4/project/view/test/HomePage.java
+++ src/main/java/fr/tse/info4/project/view/test/HomePage.java
@@ -35,6 +35,9 @@ public class HomePage extends JFrame {
 		JLabel hello = new JLabel("Bienvenue "+(new ApiManager()).getUserNAme((int)ApiManager.getIdUser(acessToken)));
 		wellcome.add(hello);
 		
+		PageAlice alice = new PageAlice(acessToken);
+		PageBob bob = new PageBob(acessToken);
+		
 		onglets.addTab("Bienvenue", wellcome);
 
 		onglets.addTab("Alice", new JPanel());
@@ -47,7 +50,7 @@ public class HomePage extends JFrame {
 			@Override
 			public void stateChanged(ChangeEvent arg0) {
 				// TODO Auto-generated method stub
-				changeTab(onglets,acessToken,0);
+				changeTab(onglets,alice,bob);
 			}
 			
 		});
@@ -66,6 +69,9 @@ public class HomePage extends JFrame {
 		JLabel hello = new JLabel("Bienvenue "+(new ApiManager()).getUserNAme(id));
 		wellcome.add(hello);
 		
+		PageAlice alice = new PageAlice(id);
+		PageBob bob = new PageBob(id);
+		
 		onglets.addTab("Bienvenue", wellcome);
 
 		onglets.addTab("Alice", new JPanel());
@@ -77,7 +83,7 @@ public class HomePage extends JFrame {
 			@Override
 			public void stateChanged(ChangeEvent arg0) {
 				// TODO Auto-generated method stub
-				changeTab(onglets,null,id);
+				changeTab(onglets,alice,bob);
 			}
 			
 		});
@@ -88,41 +94,20 @@ public class HomePage extends JFrame {
 		this.setVisible(true);
 	}
 
-	
-	public void changeTab(JTabbedPane onglets,String acess, int id){
+	public void changeTab(JTabbedPane onglets,PageAlice panelAlice,PageBob panelBob){
 		int index = onglets.getSelectedIndex();
 		switch(index){
 		case 1:
 			JPanel alice = (JPanel) onglets.getComponentAt(1);
 			alice.removeAll();
-			try {
-				if(acess==null){
-					alice.add(new PageAlice(id));
-				}else{
-					alice.add(new PageAlice(acess));
-				}
-				
-			} catch (IOException | URISyntaxException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+			alice.add(panelAlice);
 			break;
 		case 2:
 			JPanel bob = (JPanel) onglets.getComponentAt(2);
 			bob.removeAll();
-			try {
-				if(acess==null){
-					bob.add(new PageBob(id));
-				}else{
-					bob.add(new PageBob(acess));
-				}
-			} catch (IOException | URISyntaxException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+			bob.add(panelBob);
 			break;
 		}
-		
 	}
 
 	public static void main(String[] args) throws IOException {
